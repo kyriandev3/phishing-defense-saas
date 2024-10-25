a8ca3c104e1f432f2b70d359e38661ec84be717a
kyriandev3 <stevenkyrian137@gmail.com>
2024-10-25 08:13:31 +0100

    Removed function to save vectorizer as it;s impacting the entire project. Only needed for a test run. I'll now create a separate file to test run test_email.csv

--- src/ai_model.py
+++ src/ai_model.py
@@ -25,10 +25,6 @@ y = data['label']
 # Split into train and test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Vectorize the text using TF-IDF
-vectorizer = TfidfVectorizer()
-X_train_tfidf = vectorizer.fit_transform(X_train)
-
 # Train model
 model = LogisticRegression()
 model.fit(X_train, y_train)
